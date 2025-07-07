adbb4e23fa041946f5c7ca037f910275e8a9d0db
Rizel25 <rjmallari975@gmail.com>
2025-07-07 17:41:26 +0800

    added page auth callback

---
+++ pages/auth/callback.vue
@@ -0,0 +1,16 @@
+<template>
+  <div classe="p-4">signing you in...</div>
+</template>
+<script>
+export default {
+  async mounted() {
+    try {
+      await this.$auth.fetchUser();
+      this.$router.replace("/");
+    } catch (err) {
+      console.err("login failed:", error);
+      this.$router.push("/auth/signin");
+    }
+  },
+};
+</script>
